f7f2ec55c9f071a5517634d5356c5deab471ee45
Shreyas Employee <shreywankhade.95@gmail.com>
2026-03-26 20:16:11 +0000

    updated main.py

--- src/app/main.py
+++ src/app/main.py
@@ -9,3 +9,11 @@ app = FastAPI(
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Notes App"}
+
+@app.on_event("startup")
+async def startup_event():
+    print("INFO:     Application startup complete.")
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    print("INFO:     Application shutdown complete.")
