77a1edf2383106ec8d9a46cccddd0758b3cb8e0b
Shreyas Employee <shreywankhade.95@gmail.com>
2026-03-26 19:48:21 +0000

    update python app

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 
-
 app = FastAPI(
   title = "Notes App",
   version = "1.0.0",
   description = "lets try building a notes app"
 )
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Notes App"}
